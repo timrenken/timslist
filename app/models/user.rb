97433368738813a72d6ca842bef9eacf46f47965
Tim Renken <tim@timrenken.io>
2015-04-10 11:15:05 -0500

    generated places scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :name
+  has_many :places
 end
